ba7d14947d4cdd4cc6b84160c7db11a4a3d60e6c
aneeshmunnam <munnamaneesh2@gmail.com>
2024-05-19 23:03:45 -0400

    Add sweat functionality using useRef hook

--- src/components/Sweat.js
+++ src/components/Sweat.js
@@ -1,19 +1,61 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import TaskList from "../common/TaskList";
 
 export default function Sweat({selectedDate}) {
 
-    const [sweats, setSweats] = useState([]);
+    const workout = useRef('');
+    const weight = useRef('');
 
-    const [sweat, setSweat] = useState({
-        "id": 0,
-        "workout": "",
-        "Weight/reps": "",
-        "status": false
-    });
+    const [sweats, setSweats] = useState([]);
 
     useEffect(() => {
         setSweats([]);
     }, [selectedDate]);
+
+    const handleSweat = (e) => {
+        if ((workout.current && workout.current.length === 0) || 
+                        (weight.current && weight.current.length === 0)) return;
+        const sweatTask = {
+            "id": Math.floor(Math.random() * 100000) + 1,
+            "task": workout.current.value,
+            "weight": weight.current.value,
+            "status": false
+        };
+        setSweats([
+            ...sweats,
+            sweatTask
+        ]);
+        workout.current.value = '';
+        weight.current.value = '';
+        console.log(sweats);
+        e.preventDefault();
+    };
+
+    const handleWorkoutStatus = (id) => {
+        setSweats(sweats.map((task) => {
+            if (task.id === id) {
+                task.status = !task.status;
+                return task;
+            } else {
+                return task;
+            }
+        }));
+    }
+
+    const handleDeleteWorkoutTask = (id) => {
+        setSweats(sweats.filter(task => task.id !== id));
+    }
+
+    const handleEditWorkoutTask = (editTask) => {
+        setSweats(sweats.map((task) => {
+            if (task.id === editTask.id) {
+                task.task=editTask.task;
+                return task;
+            } else {
+                return task;
+            }
+        }));
+    }
 
     return (
         <div className="sweat">
@@ -22,14 +64,23 @@ export default function Sweat({selectedDate}) {
                 <div className="col">
                     <input type="text"
                     className="form-control"
+                    ref={workout}
                     placeholder="Workout" />
                 </div>
                 <div className="col">
                     <input type="text" 
+                    ref={weight}
                     className="form-control"
                     placeholder="Weight" />
                 </div>
+                <div className="col">
+                    <button className="btn btn-primary" onClick={handleSweat}>Add Workout</button>
+                </div>
             </div>
+            <TaskList tasks={sweats} 
+                    handleStatus={handleWorkoutStatus} 
+                    handleDeleteTask={handleDeleteWorkoutTask} 
+                    handleEditTask={handleEditWorkoutTask} />
         </div>
     );
 }
